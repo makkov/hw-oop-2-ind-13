3bce3b20645901064470102433f806e1a90c3dbe
maks <maxim_86nva@mail.ru>
2023-08-12 12:09:23 +0300

    init project

---
+++ src/com/hw/maxim/Bicycle.java
@@ -0,0 +1,11 @@
+package com.hw.maxim;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
---
+++ src/com/hw/maxim/Car.java
@@ -0,0 +1,15 @@
+package com.hw.maxim;
+
+public class Car {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
